43c460c10d7a68844921dcabb927ea7cca5343dd
usharani12345 <ulangalausharani@gmail.com>
2017-05-20 16:50:37 +0530

    updated hello.html

--- app.js
+++ app.js
@@ -3,11 +3,47 @@ var app = angular.module("myApp",[]);
 app.directive("myFirstDirective",function(){
 	return{
 		restrict:'EACM',
-		template:"<div><h1>Customer Directive</h1><p>This Is My Paragrapg</p></div>",
-		replace:true
-		
+		templateUrl:'EmployeeDetails.html',
+		replace:true,
+		scope:{
+			headerName:'@',
+			employeeDetails: '=',
+			changeEmployeeName:'&'
+			
+		},
+	
+				link:function(scope,elem,attrs){
+					console.log("inside post method");
+					console.log(elem);
+					console.log(attrs);
+
+				}
+				
+
 	}
 })
+
+
 app.controller("myController",["$scope",function($scope){
 	$scope.message="Good Morning ";
+	$scope.header="Techminds Learinings";
+	$scope.employeeData= [{
+		name:"usha",
+		email:"usha@gmail.com"
+	},
+	{
+		name:"john",
+		email:"john@gmail.com"
+	},
+	{
+		name:"deo",
+		email:"deo@gmail.com"
+	}]
+	$scope.changeEmployeeName = function(){
+		$scope.header= "Techminds Solutions";
+		
+	}
+	$scope.customer=["john","deo",25];
+
+
 }])
